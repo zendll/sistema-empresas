038c5b9a7e5774f725d10f4a08d23fe5a2b1690f
zx <gokukk43@gmail.com>
2024-11-03 14:39:06 -0300

    db empresas

---
+++ db.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `empresas` (
+    `ID` INT AUTO_INCREMENT PRIMARY KEY,
+    `Nome` VARCHAR(100) NOT NULL,
+    `Dono` VARCHAR(50) NOT NULL,
+    `Saldo` FLOAT NOT NULL DEFAULT 0,
+    `EntradaX` FLOAT NOT NULL,
+    `EntradaY` FLOAT NOT NULL,
+    `EntradaZ` FLOAT NOT NULL,
+    `Interior` INT NOT NULL,
+    `World` INT NOT NULL
+);
