f73ba5c45411bf68d8f22459bc9aa2003c621f4b
zx <gokukk43@gmail.com>
2024-11-03 14:49:50 -0300

    dbemp.sql

--- db.sql
+++ db.sql
@@ -2,7 +2,7 @@ CREATE TABLE `empresas` (
     `ID` INT AUTO_INCREMENT PRIMARY KEY,
     `Nome` VARCHAR(100) NOT NULL,
     `Dono` VARCHAR(50) NOT NULL,
-    `Saldo` FLOAT NOT NULL DEFAULT 0,
+    `Saldo` INT NOT NULL DEFAULT 0,
     `EntradaX` FLOAT NOT NULL,
     `EntradaY` FLOAT NOT NULL,
     `EntradaZ` FLOAT NOT NULL,
